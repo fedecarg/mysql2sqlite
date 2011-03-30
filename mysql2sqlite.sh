c5fddc71f7331d1429152e83cedb34c4b7db31c5
fedecarg <fedecarg@gmail.com>
2011-03-30 15:05:43 +0100

    addded DB_NAME

--- mysql2sqlite.sh
+++ mysql2sqlite.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 if [ "$1" == "" ]; then
-  echo "Usage: $0 <filename> eg. name-version.sql"
+  echo "Usage: $0 <filename> eg. filename-version.sql"
   exit 0
 fi
 
 DB_USER="root"
 DB_PASS="root"
 DB_HOST="127.0.0.1"
+DB_NAME="example"
 
-mysqldump -u $DB_USER -p$DB_PASS -h $DB_HOST --skip-add-locks --compatible=ansi --default-character-set=binary monsters_loc > mysql/$1
+mysqldump -u $DB_USER -p$DB_PASS -h $DB_HOST --skip-add-locks --compatible=ansi --default-character-set=binary $DB_NAME > mysql/$1
 cp mysql/$1 mysql/latest.sql
 
 ./convert.sh mysql/$1 sqlite/$1
